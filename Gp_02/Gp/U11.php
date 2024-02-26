10b47c9f149879fd40bc907e0f950869edf41c73
stef de feijter <stef.defeijter43@gmail.com>
2024-02-26 10:40:59 +0100

    lege ploegen paginas

--- Gp_02/Gp/U11.php
+++ Gp_02/Gp/U11.php
@@ -77,38 +77,7 @@
 
 
   
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center justify-content-center">
-    <div class="container" data-aos="fade-up">
 
-      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-        <div class="col-xl-6 col-lg-8">
-          <h1>Welkom bij KVVE Massemen admin pagina<span>.</span></h1>
-        </div>
-      </div>
-
-      <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
-        <div class="col-xl-2 col-md-4">
-          <div class="icon-box">
-            <i class="ri-bar-chart-box-line"></i>
-            <h3><a href="zoeken.php">Zoeken</a></h3>
-          </div>
-        </div>
-        <div class="col-xl-2 col-md-4">
-          <div class="icon-box">
-            <i class="ri-calendar-todo-line"></i>
-            <h3><a href="overzichtspelers.php">Overzicht spelers</a></h3>
-          </div>
-        </div>
-        <div class="col-xl-2 col-md-4">
-          <div class="icon-box">
-            <i class="ri-calendar-todo-line"></i>
-            <h3><a href="overzichtbericht.php">Overzicht berichten</a></h3>
-          </div>
-        </div>
-      </div>
-  </section>
-    
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="footer-top">

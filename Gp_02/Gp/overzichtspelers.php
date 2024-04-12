159680aa231634356ad7349b7e1399268798db1e
LiLzzzzz <stef.de.feijter@gmail.com>
2024-04-12 13:39:11 +0200

    minor

--- Gp_02/Gp/overzichtspelers.php
+++ Gp_02/Gp/overzichtspelers.php
@@ -108,10 +108,10 @@
                     <?php echo "</td><td tyle='text-align: center;'>";
                     ?>
                       <form name='Wijzigen' method='post' action='updatepage.php?actieverander&spelerid=<?php echo $id;?>'><input style="margin: auto  ;"type='submit' name='update' id='update' value='Wijzig'></form>
-                    <?php echo "</td></tr>";
+                    <?php echo "</td>";
                     ?>
                       <form name='Verwijderen' method='post' action='verwijder.php?actiemeerinfo&spelerid=<?php echo $id;?>'><input style="margin: auto;" type='submit' name='Verwijderen' id='Verwijderen' value='Verwijderen'></form>
-                    <?php echo "</td><td>";
+                    <?php echo "</td><td></tr>";
                     
                 }
                 echo "</table></div>"; 
